08b0219927eaf674a1dd1f425de2d90e6a378714
pcoklo <petarcoklo@gmail.com>
2015-06-16 02:48:37 +0200

    inital

---
+++ ArduinoInternetClock.ino
@@ -0,0 +1,99 @@
+#include <LiquidCrystal.h>
+#include <EthernetUdp.h>
+#include <Ethernet.h>
+#include <RTClib.h>
+#include <Wire.h>
+#include <SPI.h>
+
+LiquidCrystal lcd(6,7,8,9,11,12);
+RTC_DS1307 rtc;
+
+DateTime now, old;
+
+uint32_t timerTime=0, oldTimer;
+
+bool timerFlag=0, stopWatchFlag=0;
+
+void setup(){
+	Serial.begin(57600);
+	lcd.begin(16, 2);
+	Wire.begin();
+	rtc.begin();
+	pinMode(13,OUTPUT);
+	digitalWrite(13,HIGH);
+
+	if (!rtc.isrunning())
+	rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
+void loop(){
+	now = rtc.now();
+	if(old.second() != now.second()) lcdPrintDateTime(now, 0);
+
+
+	if(timerFlag){
+		TimeSpan timer(timerTime);
+		if(timerTime != oldTimer) lcdPrintTimeSpan(timer, 1);
+		timerTime++;
+	}
+
+	if(stopWatchFlag){
+		TimeSpan
+	}
+
+	if(Serial.available()){
+		String dataString = Serial.readStringUntil('\n');
+		lcd.setCursor(0,1);
+		lcd.print(dataString);
+	}
+}
+
+void serialEvent(){
+	String dataString = Serial.readStringUntil('\n');
+	lcd.setCursor(0,1);
+	lcd.print(dataString);
+}
+
+void lcdPrintDateTime(DateTime &time, int line){
+	old = time;
+	lcd.setCursor(0,line);
+	String dataString;
+
+	if(time.hour() < 10) dataString += '0';
+	dataString += time.hour(); dataString += ':';
+	if(time.minute() < 10) dataString += '0';
+	dataString += time.minute(); dataString += ':';
+	if(time.second() < 10) dataString += '0';
+	dataString += time.second();
+
+	if(time.second()%10 >= 0 && time.second()%10 <= 7){
+		dataString += "   ";
+		if(time.day() < 10) dataString += '0';
+		dataString += time.day(); dataString += '/';
+		if(time.month() < 10) dataString += '0';
+		dataString += time.month();
+	}
+	else{
+		dataString += "    ";
+		dataString += time.year();
+	}
+	lcd.print(dataString);
+}
+
+void lcdPrintTimeSpan(TimeSpan &time, int line){
+	lcd.setCursor(0,line);
+	String dataString;
+
+	if(time.day() > 0){
+		dataString += time.day(); dataString += ' ';
+	}
+	if(time.hour() > 0){
+		dataString += time.hour(); dataString += ':';
+	}
+	
+	if(time.minute() < 10) dataString += '0';
+	dataString += time.minute(); dataString += ':';
+	if(time.second() < 10) dataString += '0';
+	dataString += time.second(); dataString += "  ";
+	lcd.print(dataString);
+}
