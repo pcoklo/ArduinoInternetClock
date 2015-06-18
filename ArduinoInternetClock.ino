d8dde9d30ccb62543ac4be7f2789ac6e70fd6a4a
pcoklo <petarcoklo@gmail.com>
2015-06-18 21:02:38 +0200

    dodani komentari

--- ArduinoInternetClock.ino
+++ ArduinoInternetClock.ino
@@ -1,33 +1,33 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h>  //lib za lcd
 #include <EthernetUdp.h>
 #include <Ethernet.h>
-#include <RTClib.h>
-#include <Wire.h>
-#include <SPI.h>
+#include <RTClib.h> 
+#include <Wire.h> //komunikacija preko i2c sa rtc
+#include <SPI.h>  //komunikacija sa eternet preko spi
 
-byte mac[] = {
+byte mac[] = {  // bite array macadresa
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 
 LiquidCrystal lcd(2,3,4,5,7,8);
 EthernetServer server(80);
 RTC_DS1307 rtc;
-bool clearIP=1;
-int updateTime=0;
+bool clearIP=1; //za brisanje ip adrese sa lcda
+int updateTime=0; //za update vremena preko ntp servera
 
-unsigned long tick, tack;
-unsigned int localPort = 8888;
-char timeServer[] = "time.nist.gov";
+unsigned long tick, tack; //variable provjeravaju jeli prošla cca sekunda
+unsigned int localPort = 8888;  //port za ntp
+char timeServer[] = "time.nist.gov";  //sa te stranice kupimo vrijeme
 const int NTP_PACKET_SIZE = 48;
 byte packetBuffer[NTP_PACKET_SIZE];
 unsigned long ntpOfset=0;
 
-struct clock{
-  DateTime time, old;
-  bool display = false;
-  int line;
+struct clock{ //struktura za jednostavnije korištenje rtca i ispisa na lcd
+  DateTime time, old; //time = trenuno vrijeme, old je prošlo
+  bool display = false; //dozvoli ispis
+  int line; //na koju lijinu ispisuje, 0-prva, 1-druga
 
-  String data(){
+  String data(){  //funkcija vraća uređeni string znakova sata
     String dataString;
     if(time.hour() < 10) dataString += '0';
     dataString += time.hour(); dataString += ':';
@@ -35,8 +35,10 @@ struct clock{
     dataString += time.minute(); dataString += ':';
     if(time.second() < 10) dataString += '0';
     dataString += time.second();
+    //dodaje 0 za lijepši prikaz...
 
     if(time.second()%10 >= 0 && time.second()%10 <= 7){
+      //između 0 i 7 sekunde dodaj datum
       dataString += "   ";
       if(time.day() < 10) dataString += '0';
       dataString += time.day(); dataString += '/';
@@ -44,6 +46,7 @@ struct clock{
       dataString += time.month();
     }
     else{
+      //između 8 i nulte dodaj godinu 
       dataString += "    ";
       dataString += time.year();
     }
@@ -53,12 +56,13 @@ struct clock{
 
   void printToLcd(int linet){
     line = linet;
-    time = rtc.now();
+    time = rtc.now(); //osvježi trenutno vrijeme
     if (display){
       if(old.second() != time.second()){
-        lcd.setCursor(0,line);
-        lcd.print(data());
-        old = time;
+        //ako se vrijeme promjenilo od zadnjeg ispisa, ispiši novo vrjeme
+        lcd.setCursor(0,line);  //ma,kesto lursor na početak zadane linije
+        lcd.print(data());  //ispiši vrijeme na display
+        old = time; //novo vrijeme je staro vrijeme
       }
     }
   }
@@ -67,11 +71,12 @@ struct clock{
 struct timer{
   unsigned long timerTime, old;
   bool display = false;
-  bool enable = 0;
-  bool off = 0;
+  bool enable = 0;  //1 ako je pozvan
+  bool off = 0; //1 ako je istekao
   int line;
 
   void begin(unsigned long time){
+    // poziva se prilikom pokretanja timera
     timerTime = time;
   }
 
@@ -90,6 +95,8 @@ struct timer{
     dataString += time.minute(); dataString += ':';
     if(time.second() < 10) dataString += '0';
     dataString += time.second();
+    //popuni atrin praznimama do veličine retka dipleja
+    //za čišći ispis
     while (dataString.length() <16) dataString+=' ';
 
     return dataString;
@@ -108,6 +115,7 @@ struct timer{
 };
 
 struct stopwatch{
+  //štoperica
   unsigned long startMillis, currentMillis;
   TimeSpan time;
   bool display = false;
@@ -174,6 +182,7 @@ struct alarm{
 
   String data(){
     String dataString = "ALARM: ";
+    //ispis alarm na lcd da znam da smo postavili alarm
     
     if(time.hour() < 10) dataString += '0';
     dataString += time.hour(); dataString += ':';
@@ -197,7 +206,7 @@ struct alarm{
   }
 };
 
-EthernetUDP Udp;
+EthernetUDP Udp;  //za pozivanje postavljanja vremena
 stopwatch stopwatch1;
 timer timer1;
 alarm alarm1;
@@ -205,8 +214,7 @@ clock clock1;
 
 void setup(){
   lcd.begin(16, 2);
-  Serial.begin(9600);
-  Wire.begin();
+  Wire.begin(); 
   rtc.begin();
   pinMode(9,OUTPUT);
   pinMode(6, OUTPUT);
@@ -214,17 +222,21 @@ void setup(){
   digitalWrite(9,HIGH);
 
   if (!rtc.isrunning()) rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
   lcd.print("DHCP...");
+  
   lcd.setCursor(0,1);
   Ethernet.begin(mac);
   server.begin();
   lcd.print(Ethernet.localIP());
   clock1.display=1;
   clock1.printToLcd(0);
+  //begin fuunkcije započinju rad sa stvarima
 }
 
 void loop(){
   tick=millis();
+  //provjerava jeli prošla sekunda
   if(tick >= (tack+1000)){
     if(timer1.timerTime){
       timer1.timerTime --;
@@ -233,20 +245,24 @@ void loop(){
     tack = millis();tack -= millis()%10;
     clock1.printToLcd(0);
 
+    //pali alarm ako su minute i sati jednke i ako je 00 sekundi u satu sata 
     if(alarm1.time.hour() == clock1.time.hour() && alarm1.time.minute() == clock1.time.minute() && clock1.time.second()==0){
       alarm1.off = 1;
     }
   }
 
   if(digitalRead(A0) == LOW){
+    //ako je tipka pritisnuta
     delay(10);
     if(alarm1.off){
+      //ako je alarm uključen, gasi ga
       alarm1.off = 0;
       alarm1.display=0;
       lcd.setCursor(0,alarm1.line);
       lcd.print("                ");
     }
     if(timer1.off){
+      //ako je timer uključen, gasi ga
       timer1.off = 0;
       timer1.display=0;
       lcd.setCursor(0,timer1.line);
@@ -269,8 +285,10 @@ void loop(){
         client.println("<html>");
         client.println("Online Clock!");
         client.println("<br />");
-        if(updateTime) updateTime++;
+        //http stvari koje nerazumijem, a trebaju
+        if(updateTime) updateTime++; //ako je update time veći od nule povečaj ga još jednom zbog razloga
         if(clearIP){
+          //ako piše ip adresa, obriši ju
           clearIP = 0;
           lcd.setCursor(0,1);
           lcd.print("                ");
@@ -285,7 +303,7 @@ void loop(){
           alarm1.printToLcd(1);
           client.println(alarm1.data());
           client.println("<br />");
-        }
+        } //uključivanje alarma putem weba
         else if(dataString.startsWith("timer")){
           String tempString=dataString.substring(dataString.indexOf("(")+1,dataString.indexOf(")"));
           long time = tempString.toInt();
@@ -295,7 +313,7 @@ void loop(){
           client.println("TIMER: ");
           client.println(timer1.data());
           client.println("<br />");
-        }
+        } //uključivanje alarma putem weba
         else if(dataString.startsWith("sw")){
           String tempString=dataString.substring(dataString.indexOf(".")+1,dataString.length());
           if(tempString == "start"){
@@ -339,13 +357,12 @@ void loop(){
       noTone(6);
       digitalWrite(9,1);
     }
-  }
+  } // ako se aktivir alarm ili timer, svakih cca pol sec, toglaj backlight i pištanje
   else{
     noTone(6);
     digitalWrite(9,1);
   }
   if(updateTime == 2){
     updateTime=0;
-    Serial.println("updateTIME");
   }
 }
